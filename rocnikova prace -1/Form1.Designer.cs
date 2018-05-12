8de3a0f91056896ab0da6d6abf35cbc1bbd8abe3
Lukáš Hubálek <39216257+hicoiralukas@users.noreply.github.com>
2018-05-12 14:04:54 +0200

    Add files via upload

---
+++ rocnikova prace -1/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace rocnikova_prace__1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxZnamka1 = new System.Windows.Forms.TextBox();
+            this.textBoxZnamka2 = new System.Windows.Forms.TextBox();
+            this.textBoxZnamka5 = new System.Windows.Forms.TextBox();
+            this.textBoxZnamka4 = new System.Windows.Forms.TextBox();
+            this.textBoxZnamka3 = new System.Windows.Forms.TextBox();
+            this.lbl1znamka = new System.Windows.Forms.Label();
+            this.lbl2znamka = new System.Windows.Forms.Label();
+            this.lbl3znamka = new System.Windows.Forms.Label();
+            this.lbl4znamka = new System.Windows.Forms.Label();
+            this.lbl5znamka = new System.Windows.Forms.Label();
+            this.btnVypocet = new System.Windows.Forms.Button();
+            this.textBoxVaha1 = new System.Windows.Forms.TextBox();
+            this.textBoxVaha5 = new System.Windows.Forms.TextBox();
+            this.textBoxVaha3 = new System.Windows.Forms.TextBox();
+            this.textBoxVaha4 = new System.Windows.Forms.TextBox();
+            this.textBoxVaha2 = new System.Windows.Forms.TextBox();
+            this.lbl1Vaha = new System.Windows.Forms.Label();
+            this.lbl2Vaha = new System.Windows.Forms.Label();
+            this.lbl3Vaha = new System.Windows.Forms.Label();
+            this.lbl4Vaha = new System.Windows.Forms.Label();
+            this.lbl5Vaha = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblZnamka = new System.Windows.Forms.Label();
+            this.lblVyaha = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxZnamka1
+            // 
+            this.textBoxZnamka1.Location = new System.Drawing.Point(12, 12);
+            this.textBoxZnamka1.Name = "textBoxZnamka1";
+            this.textBoxZnamka1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxZnamka1.TabIndex = 0;
+            // 
+            // textBoxZnamka2
+            // 
+            this.textBoxZnamka2.Location = new System.Drawing.Point(12, 38);
+            this.textBoxZnamka2.Name = "textBoxZnamka2";
+            this.textBoxZnamka2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxZnamka2.TabIndex = 1;
+            // 
+            // textBoxZnamka5
+            // 
+            this.textBoxZnamka5.Location = new System.Drawing.Point(12, 116);
+            this.textBoxZnamka5.Name = "textBoxZnamka5";
+            this.textBoxZnamka5.Size = new System.Drawing.Size(100, 20);
+            this.textBoxZnamka5.TabIndex = 2;
+            // 
+            // textBoxZnamka4
+            // 
+            this.textBoxZnamka4.Location = new System.Drawing.Point(12, 90);
+            this.textBoxZnamka4.Name = "textBoxZnamka4";
+            this.textBoxZnamka4.Size = new System.Drawing.Size(100, 20);
+            this.textBoxZnamka4.TabIndex = 3;
+            // 
+            // textBoxZnamka3
+            // 
+            this.textBoxZnamka3.Location = new System.Drawing.Point(12, 64);
+            this.textBoxZnamka3.Name = "textBoxZnamka3";
+            this.textBoxZnamka3.Size = new System.Drawing.Size(100, 20);
+            this.textBoxZnamka3.TabIndex = 4;
+            // 
+            // lbl1znamka
+            // 
+            this.lbl1znamka.AutoSize = true;
+            this.lbl1znamka.Location = new System.Drawing.Point(132, 12);
+            this.lbl1znamka.Name = "lbl1znamka";
+            this.lbl1znamka.Size = new System.Drawing.Size(56, 13);
+            this.lbl1znamka.TabIndex = 5;
+            this.lbl1znamka.Text = "1. známka";
+            // 
+            // lbl2znamka
+            // 
+            this.lbl2znamka.AutoSize = true;
+            this.lbl2znamka.Location = new System.Drawing.Point(132, 41);
+            this.lbl2znamka.Name = "lbl2znamka";
+            this.lbl2znamka.Size = new System.Drawing.Size(56, 13);
+            this.lbl2znamka.TabIndex = 6;
+            this.lbl2znamka.Text = "2. známka";
+            // 
+            // lbl3znamka
+            // 
+            this.lbl3znamka.AutoSize = true;
+            this.lbl3znamka.Location = new System.Drawing.Point(132, 67);
+            this.lbl3znamka.Name = "lbl3znamka";
+            this.lbl3znamka.Size = new System.Drawing.Size(56, 13);
+            this.lbl3znamka.TabIndex = 7;
+            this.lbl3znamka.Text = "3. známka";
+            // 
+            // lbl4znamka
+            // 
+            this.lbl4znamka.AutoSize = true;
+            this.lbl4znamka.Location = new System.Drawing.Point(132, 93);
+            this.lbl4znamka.Name = "lbl4znamka";
+            this.lbl4znamka.Size = new System.Drawing.Size(56, 13);
+            this.lbl4znamka.TabIndex = 8;
+            this.lbl4znamka.Text = "4. známka";
+            // 
+            // lbl5znamka
+            // 
+            this.lbl5znamka.AutoSize = true;
+            this.lbl5znamka.Location = new System.Drawing.Point(132, 119);
+            this.lbl5znamka.Name = "lbl5znamka";
+            this.lbl5znamka.Size = new System.Drawing.Size(56, 13);
+            this.lbl5znamka.TabIndex = 9;
+            this.lbl5znamka.Text = "5. známka";
+            // 
+            // btnVypocet
+            // 
+            this.btnVypocet.BackColor = System.Drawing.Color.Lime;
+            this.btnVypocet.Location = new System.Drawing.Point(149, 205);
+            this.btnVypocet.Name = "btnVypocet";
+            this.btnVypocet.Size = new System.Drawing.Size(176, 23);
+            this.btnVypocet.TabIndex = 10;
+            this.btnVypocet.Text = "Výpočet - více desetiných míst";
+            this.btnVypocet.UseVisualStyleBackColor = false;
+            this.btnVypocet.Click += new System.EventHandler(this.btnVypocet_Click);
+            // 
+            // textBoxVaha1
+            // 
+            this.textBoxVaha1.Location = new System.Drawing.Point(205, 12);
+            this.textBoxVaha1.Name = "textBoxVaha1";
+            this.textBoxVaha1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxVaha1.TabIndex = 11;
+            // 
+            // textBoxVaha5
+            // 
+            this.textBoxVaha5.Location = new System.Drawing.Point(205, 116);
+            this.textBoxVaha5.Name = "textBoxVaha5";
+            this.textBoxVaha5.Size = new System.Drawing.Size(100, 20);
+            this.textBoxVaha5.TabIndex = 12;
+            // 
+            // textBoxVaha3
+            // 
+            this.textBoxVaha3.Location = new System.Drawing.Point(205, 64);
+            this.textBoxVaha3.Name = "textBoxVaha3";
+            this.textBoxVaha3.Size = new System.Drawing.Size(100, 20);
+            this.textBoxVaha3.TabIndex = 13;
+            // 
+            // textBoxVaha4
+            // 
+            this.textBoxVaha4.Location = new System.Drawing.Point(205, 90);
+            this.textBoxVaha4.Name = "textBoxVaha4";
+            this.textBoxVaha4.Size = new System.Drawing.Size(100, 20);
+            this.textBoxVaha4.TabIndex = 14;
+            // 
+            // textBoxVaha2
+            // 
+            this.textBoxVaha2.Location = new System.Drawing.Point(205, 38);
+            this.textBoxVaha2.Name = "textBoxVaha2";
+            this.textBoxVaha2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxVaha2.TabIndex = 15;
+            // 
+            // lbl1Vaha
+            // 
+            this.lbl1Vaha.AutoSize = true;
+            this.lbl1Vaha.Location = new System.Drawing.Point(311, 15);
+            this.lbl1Vaha.Name = "lbl1Vaha";
+            this.lbl1Vaha.Size = new System.Drawing.Size(83, 13);
+            this.lbl1Vaha.TabIndex = 16;
+            this.lbl1Vaha.Text = "Váha 1. známky";
+            // 
+            // lbl2Vaha
+            // 
+            this.lbl2Vaha.AutoSize = true;
+            this.lbl2Vaha.Location = new System.Drawing.Point(311, 41);
+            this.lbl2Vaha.Name = "lbl2Vaha";
+            this.lbl2Vaha.Size = new System.Drawing.Size(83, 13);
+            this.lbl2Vaha.TabIndex = 17;
+            this.lbl2Vaha.Text = "Váha 2. známky";
+            // 
+            // lbl3Vaha
+            // 
+            this.lbl3Vaha.AutoSize = true;
+            this.lbl3Vaha.Location = new System.Drawing.Point(311, 67);
+            this.lbl3Vaha.Name = "lbl3Vaha";
+            this.lbl3Vaha.Size = new System.Drawing.Size(83, 13);
+            this.lbl3Vaha.TabIndex = 18;
+            this.lbl3Vaha.Text = "Váha 3. známky";
+            // 
+            // lbl4Vaha
+            // 
+            this.lbl4Vaha.AutoSize = true;
+            this.lbl4Vaha.Location = new System.Drawing.Point(311, 93);
+            this.lbl4Vaha.Name = "lbl4Vaha";
+            this.lbl4Vaha.Size = new System.Drawing.Size(83, 13);
+            this.lbl4Vaha.TabIndex = 19;
+            this.lbl4Vaha.Text = "Váha 4. známky";
+            // 
+            // lbl5Vaha
+            // 
+            this.lbl5Vaha.AutoSize = true;
+            this.lbl5Vaha.Location = new System.Drawing.Point(311, 119);
+            this.lbl5Vaha.Name = "lbl5Vaha";
+            this.lbl5Vaha.Size = new System.Drawing.Size(83, 13);
+            this.lbl5Vaha.TabIndex = 20;
+            this.lbl5Vaha.Text = "Váha 5. známky";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(149, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 23);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Výpočet - méně desetiných míst";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblZnamka
+            // 
+            this.lblZnamka.AutoSize = true;
+            this.lblZnamka.Location = new System.Drawing.Point(12, 139);
+            this.lblZnamka.Name = "lblZnamka";
+            this.lblZnamka.Size = new System.Drawing.Size(161, 13);
+            this.lblZnamka.TabIndex = 22;
+            this.lblZnamka.Text = "Známka může být v rozmezí 1-5!";
+            // 
+            // lblVyaha
+            // 
+            this.lblVyaha.AutoSize = true;
+            this.lblVyaha.Location = new System.Drawing.Point(202, 139);
+            this.lblVyaha.Name = "lblVyaha";
+            this.lblVyaha.Size = new System.Drawing.Size(153, 13);
+            this.lblVyaha.TabIndex = 23;
+            this.lblVyaha.Text = "Váha může být v rozmezí 1-10!";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(149, 264);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 23);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "Vymazat veškeré hodnoty";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 407);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblVyaha);
+            this.Controls.Add(this.lblZnamka);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl5Vaha);
+            this.Controls.Add(this.lbl4Vaha);
+            this.Controls.Add(this.lbl3Vaha);
+            this.Controls.Add(this.lbl2Vaha);
+            this.Controls.Add(this.lbl1Vaha);
+            this.Controls.Add(this.textBoxVaha2);
+            this.Controls.Add(this.textBoxVaha4);
+            this.Controls.Add(this.textBoxVaha3);
+            this.Controls.Add(this.textBoxVaha5);
+            this.Controls.Add(this.textBoxVaha1);
+            this.Controls.Add(this.btnVypocet);
+            this.Controls.Add(this.lbl5znamka);
+            this.Controls.Add(this.lbl4znamka);
+            this.Controls.Add(this.lbl3znamka);
+            this.Controls.Add(this.lbl2znamka);
+            this.Controls.Add(this.lbl1znamka);
+            this.Controls.Add(this.textBoxZnamka3);
+            this.Controls.Add(this.textBoxZnamka4);
+            this.Controls.Add(this.textBoxZnamka5);
+            this.Controls.Add(this.textBoxZnamka2);
+            this.Controls.Add(this.textBoxZnamka1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxZnamka1;
+        private System.Windows.Forms.TextBox textBoxZnamka2;
+        private System.Windows.Forms.TextBox textBoxZnamka5;
+        private System.Windows.Forms.TextBox textBoxZnamka4;
+        private System.Windows.Forms.TextBox textBoxZnamka3;
+        private System.Windows.Forms.Label lbl1znamka;
+        private System.Windows.Forms.Label lbl2znamka;
+        private System.Windows.Forms.Label lbl3znamka;
+        private System.Windows.Forms.Label lbl4znamka;
+        private System.Windows.Forms.Label lbl5znamka;
+        private System.Windows.Forms.Button btnVypocet;
+        private System.Windows.Forms.TextBox textBoxVaha1;
+        private System.Windows.Forms.TextBox textBoxVaha5;
+        private System.Windows.Forms.TextBox textBoxVaha3;
+        private System.Windows.Forms.TextBox textBoxVaha4;
+        private System.Windows.Forms.TextBox textBoxVaha2;
+        private System.Windows.Forms.Label lbl1Vaha;
+        private System.Windows.Forms.Label lbl2Vaha;
+        private System.Windows.Forms.Label lbl3Vaha;
+        private System.Windows.Forms.Label lbl4Vaha;
+        private System.Windows.Forms.Label lbl5Vaha;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblZnamka;
+        private System.Windows.Forms.Label lblVyaha;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ rocnikova prace -1/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rocnikova_prace__1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnVypocet_Click(object sender, EventArgs e)
+        {
+            //pole vstupů známek
+            TextBox[] vstupyZnamek = { textBoxZnamka1, textBoxZnamka2, textBoxZnamka3, textBoxZnamka4, textBoxZnamka5 };
+            //pole vstupů váh
+            TextBox[] vstupyVah = { textBoxVaha1, textBoxVaha2, textBoxVaha3, textBoxVaha4, textBoxVaha5 };
+            //pole známek
+            int[] znamky = new int[5];
+            //pole váh
+            int[] vahy = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                String vstupZnamka = vstupyZnamek[i].Text;
+                if (vstupZnamka == null || vstupZnamka.Equals(""))
+                {
+                    znamky[i] = -1;
+                    continue;
+                }
+                int znamka;
+                if (!int.TryParse(vstupyZnamek[i].Text, out znamka) || znamka <= 0 || znamka > 5)
+                {
+                    MessageBox.Show("Byla špatne zadána známka č. " + (i + 1) + ", známka může být v rozmezí 1-5");
+                    return;
+                }
+                znamky[i] = znamka;
+                String vstupVaha = vstupyVah[i].Text;
+                if (vstupVaha == null || vstupVaha.Equals(""))
+                {
+                    vahy[i] = -1;
+                    continue;
+                }
+                int vaha;
+                if (!int.TryParse(vstupVaha, out vaha) || vaha <= 0 || vaha > 10)
+                {
+                    MessageBox.Show("Byla špatne zadána váha známky č. " + (i + 1) + ", váha může být v rozmezí 1-10");
+                    return;
+                }
+                vahy[i] = vaha;
+            }
+            double celkova_vaha = 0;
+            double vypocet_prumeru = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                double znamka = znamky[i];
+                double vaha = vahy[i];
+                if (znamka == -1 || vaha == -1)
+                {
+                    continue;
+                }
+                    vypocet_prumeru += znamka * vaha;
+                celkova_vaha += vaha;
+            }
+            double vysledek = vypocet_prumeru / celkova_vaha;
+            MessageBox.Show("Tvůj průměr je: " + vysledek);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //pole vstupů známek
+            TextBox[] vstupyZnamek = { textBoxZnamka1, textBoxZnamka2, textBoxZnamka3, textBoxZnamka4, textBoxZnamka5 };
+            //pole vstupů váh
+            TextBox[] vstupyVah = { textBoxVaha1, textBoxVaha2, textBoxVaha3, textBoxVaha4, textBoxVaha5 };
+            //pole známek
+            int[] znamky = new int[5];
+            //pole váh
+            int[] vahy = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                String vstupZnamka = vstupyZnamek[i].Text;
+                if (vstupZnamka == null || vstupZnamka.Equals(""))
+                {
+                    znamky[i] = -1;
+                    continue;
+                }
+                int znamka;
+                if (!int.TryParse(vstupyZnamek[i].Text, out znamka) || znamka <= 0 || znamka > 5)
+                {
+                    MessageBox.Show("Byla špatne zadána známka č. " + (i + 1) + ", známka může být v rozmezí 1-5");
+                    return;
+                }
+                znamky[i] = znamka;
+                String vstupVaha = vstupyVah[i].Text;
+                if (vstupVaha == null || vstupVaha.Equals(""))
+                {
+                    vahy[i] = -1;
+                    continue;
+                }
+                int vaha;
+                if (!int.TryParse(vstupVaha, out vaha) || vaha <= 0 || vaha > 10)
+                {
+                    MessageBox.Show("Byla špatne zadána váha známky č. " + (i + 1) + ", váha může být v rozmezí 1-10");
+                    return;
+                }
+                vahy[i] = vaha;
+            }
+            float celkova_vaha = 0;
+            float vypocet_prumeru = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                float znamka = znamky[i];
+                float vaha = vahy[i];
+                if (znamka == -1 || vaha == -1)
+                {
+                    continue;
+                }
+                vypocet_prumeru += znamka * vaha;
+                celkova_vaha += vaha;
+            }
+            float vysledek = vypocet_prumeru / celkova_vaha;
+            MessageBox.Show("Tvůj průměr je: " + vysledek);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBoxVaha1.Text = "";
+            textBoxVaha2.Text = "";
+            textBoxVaha3.Text = "";
+            textBoxVaha4.Text = "";
+            textBoxVaha5.Text = "";
+            textBoxZnamka1.Text = "";
+            textBoxZnamka2.Text = "";
+            textBoxZnamka3.Text = "";
+            textBoxZnamka4.Text = "";
+            textBoxZnamka5.Text = "";
+        }
+    }
+}
+
